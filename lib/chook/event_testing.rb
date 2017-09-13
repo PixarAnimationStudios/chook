1cfa8067794eea68c52771fc2d57b89f86dc61e1
Aurica <aurica@pixar.com>
2017-09-13 16:50:38 -0700

    Changed require order
    Subject must be required before TestSubject, because Subject contains constant values that TestSubject needs.

--- lib/chook/event_testing.rb
+++ lib/chook/event_testing.rb
@@ -1,21 +1,19 @@
 # foundation
 require 'chook/foundation'
 
-# namespace modules
-require 'chook/test_subjects'
-require 'chook/test_events'
-
 # subjects - must load before events.
 require 'chook/subject'
-require 'chook/subject/test_subject'
-# Chook::TestSubject.generate_classes
 
 # testing data generation
 require 'chook/subject/randomizers'
 require 'chook/subject/samplers'
 require 'chook/subject/validators'
+require 'chook/subject/test_subject'
+require 'chook/test_subjects'
 
 # events
 require 'chook/event'
 require 'chook/event/test_event'
-# Chook::TestEvent.generate_classes
+
+# namespace modules
+require 'chook/test_events'
