9783beb554fbae4f686aacdf0f5757a92b2cefde
Aurica <aurica@pixar.com>
2017-09-13 17:01:03 -0700

    Comment fix
    Fixed comments to reflect changes in require order

--- lib/chook/event_testing.rb
+++ lib/chook/event_testing.rb
@@ -3,17 +3,15 @@ require 'chook/foundation'
 
 # subjects - must load before events.
 require 'chook/subject'
-
-# testing data generation
 require 'chook/subject/randomizers'
 require 'chook/subject/samplers'
 require 'chook/subject/validators'
+
+# testing data generation
 require 'chook/subject/test_subject'
 require 'chook/test_subjects'
 
 # events
 require 'chook/event'
 require 'chook/event/test_event'
-
-# namespace modules
 require 'chook/test_events'
